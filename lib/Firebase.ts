206355e33a3268d6565c4bd2d76e185aab9086fb
Wade Pate <wadejp8@gmail.com>
2023-08-07 13:59:18 -0600

    restated the FirebaseConfig values as they are ok to have due to the rule system;

--- lib/Firebase.ts
+++ lib/Firebase.ts
@@ -3,28 +3,15 @@ import 'firebase/auth';
 import { browserLocalPersistence, initializeAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-if (
-  !process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
-  !process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
-  !process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ||
-  !process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
-  !process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
-  !process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
-  !process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
-  !process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-) {
-  throw new Error('Missing Firebase environment variables');
-}
-
 const FirebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyABQFYo9_alESQOQnGdS03xWemNnq0amBE',
+  authDomain: 'shiftscribe-db.firebaseapp.com',
+  databaseURL: 'https://shiftscribe-db-default-rtdb.firebaseio.com',
+  projectId: 'shiftscribe-db',
+  storageBucket: 'shiftscribe-db.appspot.com',
+  messagingSenderId: '322934906427',
+  appId: '1:322934906427:web:4bf150ab942f7f6769861e',
+  measurementId: 'G-5QGQ8TNS3F',
 };
 
 let FIREBASE_APP;

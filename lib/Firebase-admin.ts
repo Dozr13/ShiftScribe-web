fb1ccbf769673682f641096fcffb6a7493d4bb1e
Wade Pate <wadejp8@gmail.com>
2023-08-07 15:11:25 +0000

    might work;

--- lib/Firebase-admin.ts
+++ lib/Firebase-admin.ts
@@ -1,12 +1,25 @@
-var admin = require('firebase-admin');
+import 'dotenv/config';
+import admin, { ServiceAccount } from 'firebase-admin';
 
-var serviceAccount = require('../secret/shiftscribe-db-firebase-adminsdk-4xwdo-3914fc2e55.json');
+const serviceAccount: ServiceAccount = {
+  projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
+  clientEmail: `${process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL}`,
+  privateKey: `${process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')}`,
+};
 
-if (!admin.apps.length) {
+console.log(serviceAccount);
+console.log(
+  'process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL',
+  `${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}`,
+);
+
+try {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://shiftscribe-db-default-rtdb.firebaseio.com',
+    databaseURL: `${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}`,
   });
+} catch (error) {
+  console.error('Error initializing Firebase Admin SDK:', error);
 }
 
 export default admin;
---
+++ types/environment.d.ts
@@ -0,0 +1,19 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      STRIPE_PUBLISHABLE_KEY: string;
+      STRIPE_SECRET_KEY: string;
+      FIREBASE_PROJECT_ID: string;
+      FIREBASE_CLIENT_EMAIL: string;
+      FIREBASE_PRIVATE_KEY: string;
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      PWD: string;
+    }
+  }
+}
+
+// TODO: Ensure necessity of file
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
